46c58ad00423ea2b3b60edbf8befdd34aa67548b
gdl <guodun@iyaolian.cn>
2019-05-23 17:56:02 +0800

    1

--- bootdo/src/main/resources/static/js/appjs/so/so.js
+++ bootdo/src/main/resources/static/js/appjs/so/so.js
@@ -93,13 +93,13 @@ function load() {
                         formatter: function (value, row, index) {
                             var e = '<a class="btn btn-primary btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
                                 + row.id
-                                + '\')"><i class="fa fa-edit"></i></a> ';
+                                + '\')"><i class="fa fa-edit">编辑</i></a> ';
                             var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
                                 + row.id
-                                + '\')"><i class="fa fa-remove"></i></a> ';
+                                + '\')"><i class="fa fa-remove">删除</i></a> ';
                             var f = '<a class="btn btn-success btn-sm" href="#" title="备用"  mce_href="#" onclick="resetPwd(\''
                                 + row.id
-                                + '\')"><i class="fa fa-key"></i></a> ';
+                                + '\')"><i class="fa fa-key">备用</i></a> ';
                             return e + d;
                         }
                     }]
