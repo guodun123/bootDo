f154a711b80a01869a4c9786681c583c068db7bd
gdl <guodun@iyaolian.cn>
2019-05-23 10:16:46 +0800

    azz

--- bootdo/src/main/resources/static/js/appjs/sku/sku.js
+++ bootdo/src/main/resources/static/js/appjs/sku/sku.js
@@ -96,7 +96,7 @@ function add() {
 		title : '增加',
 		maxmin : true,
 		shadeClose : false, // 点击遮罩关闭层
-		area : [ '800px', '520px' ],
+		area : [ '95%', '95%' ],
 		content : prefix + '/add' // iframe的url
 	});
 }
@@ -106,7 +106,7 @@ function edit(id) {
 		title : '编辑',
 		maxmin : true,
 		shadeClose : false, // 点击遮罩关闭层
-		area : [ '800px', '520px' ],
+        area : [ '95%', '95%' ],
 		content : prefix + '/edit/' + id // iframe的url
 	});
 }
